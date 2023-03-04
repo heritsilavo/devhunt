c019fa1ba27a53fae6948e936517e28926c6e8c1
heritsilavo <heritsilavo4835@gmail.com>
2023-03-04 16:21:03 +0300

    second commit

--- other _pages/auth.js
+++ other _pages/auth.js
@@ -1,6 +1,29 @@
+loginShow=false;
 document.querySelector('.hide').addEventListener('click',function(e) {
-    document.querySelector('.login-container').style.animation="hide-login .5s forwards";    
+    if(window.innerWidth>=590){
+        document.querySelector('.login-container').style.animation="hide-login .5s forwards";    
+    }else{
+        document.querySelector('.login-container').style.animation="hide-login590 .5s forwards";
+    }
+    loginShow=false;
 })
 document.querySelector('.loginButt').addEventListener('click',function(e) {
-    document.querySelector('.login-container').style.animation="show-login .5s forwards";    
+    if(window.innerWidth>=590){
+        document.querySelector('.login-container').style.animation="show-login .5s forwards";     
+    }else{
+        document.querySelector('.hide').style.animation="animHide .5s forwards";
+        document.querySelector('.login-container').style.animation="show-login590 .5s forwards";
+    }
+    loginShow=true;
+})
+window.addEventListener('resize',function(e) {
+    if(window.innerWidth>=590 && loginShow){
+        document.querySelector('.login-container').style.animation="hide-login .5s forwards";
+        loginShow=false;
+    }else{
+        if(loginShow){
+            document.querySelector('.login-container').style.animation="hide-login590 .5s forwards";
+        loginShow=false;
+        }
+    }
 })
